a5cfc0282f846081dd530d57e180f96469c4d92f
Ben Morrison <benmorrison0@gmail.com>
2019-01-24 16:10:57 -0500

    Finish update item feature

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -32,7 +32,7 @@ export default class Item extends Component {
         <div className="buttonList">
           <Link
             href={{
-              pathname: 'update',
+              pathname: '/update',
               query: { id: item.id }
             }}
           >
