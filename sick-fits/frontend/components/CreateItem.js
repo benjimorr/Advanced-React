bd480018bcb5b3377806f1d8dd56e0ba4d08c685
Ben Morrison <benmorrison0@gmail.com>
2019-01-24 12:59:34 -0500

    Add ability to upload images to cloudinary when creating a new item

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -41,6 +41,27 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/benjimorr/image/upload',
+      {
+        method: 'POST',
+        body: data
+      }
+    );
+
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -55,11 +76,30 @@ class CreateItem extends Component {
               Router.push({
                 pathname: '/item',
                 query: { id: res.data.createItem.id }
-              })
+              });
             }}
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="image">
+                Image
+                <input
+                  type="file"
+                  id="image"
+                  name="image"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    src={this.state.image}
+                    alt="Upload Preview"
+                    width="200"
+                  />
+                )}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
