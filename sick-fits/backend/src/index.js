675d765a5c76036556bd1b64d6e102f8bb38c1d2
Ben Morrison <benmorrison0@gmail.com>
2019-01-30 13:54:37 -0500

    Create User component to grab currently logged in User, make Signin component to sign into account

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const createServer = require('./createServer');
 const db = require('./db');
@@ -6,7 +7,19 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO: Use express middleware to populate current user
+
+// Decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req so that it can be accessed later
+    req.userId = userId;
+  }
+
+  next();
+});
 
 server.start(
   {
--- sick-fits/frontend/components/Signin.js
+++ sick-fits/frontend/components/Signin.js
@@ -5,13 +5,9 @@ import Form from './styles/Form';
 import Error from './ErrorMessage';
 import { CURRENT_USER_QUERY } from './User';
 
-const SIGNUP_MUTATION = gql`
-  mutation SIGNUP_MUTATION(
-    $email: String!
-    $name: String!
-    $password: String!
-  ) {
-    signup(email: $email, name: $name, password: $password) {
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    signin(email: $email, password: $password) {
       id
       email
       name
@@ -19,10 +15,9 @@ const SIGNUP_MUTATION = gql`
   }
 `;
 
-class Signup extends Component {
+class Signin extends Component {
   state = {
     email: '',
-    name: '',
     password: ''
   };
 
@@ -33,21 +28,21 @@ class Signup extends Component {
   render() {
     return (
       <Mutation
-        mutation={SIGNUP_MUTATION}
+        mutation={SIGNIN_MUTATION}
         variables={this.state}
         refetchQueries={[{ query: CURRENT_USER_QUERY }]}
       >
-        {(signup, { loading, error }) => (
+        {(signin, { loading, error }) => (
           <Form
             method="post"
             onSubmit={async e => {
               e.preventDefault();
-              await signup();
-              this.setState({ email: '', name: '', password: '' });
+              await signin();
+              this.setState({ email: '', password: '' });
             }}
           >
             <fieldset disabled={loading} aria-busy={loading}>
-              <h2>Sign Up for an Account</h2>
+              <h2>Sign In to Your Account</h2>
               <Error error={error} />
               <label htmlFor="email">
                 Email
@@ -57,17 +52,6 @@ class Signup extends Component {
                   placeholder="Email"
                   required
                   value={this.state.email}
-                  onChange={this.saveToState}
-                />
-              </label>
-              <label htmlFor="name">
-                Name
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Full Name"
-                  required
-                  value={this.state.name}
                   onChange={this.saveToState}
                 />
               </label>
@@ -83,7 +67,7 @@ class Signup extends Component {
                 />
               </label>
 
-              <button type="submit">Sign Up</button>
+              <button type="submit">Sign In</button>
             </fieldset>
           </Form>
         )}
@@ -92,4 +76,4 @@ class Signup extends Component {
   }
 }
 
-export default Signup;
+export default Signin;
