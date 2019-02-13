90ce4a7de8fa6c090159376eea57b421dc949129
Ben Morrison <benmorrison0@gmail.com>
2019-02-13 15:16:10 -0500

    Create permissions page with new users Query to view all users permissions

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -21,6 +21,18 @@ server.express.use((req, res, next) => {
   next();
 });
 
+// Create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+  // If they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    '{ id, name, email, permissions }'
+  );
+  req.user = user;
+  next();
+});
+
 server.start(
   {
     cors: {
