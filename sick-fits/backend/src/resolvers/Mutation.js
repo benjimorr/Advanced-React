ea866d0d7d16769624b3e22797224b5e4fb8fa67
Ben Morrison <benmorrison0@gmail.com>
2019-02-13 14:19:57 -0500

    Add relationship between User and Item, make user sign in for /sell page

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -7,11 +7,21 @@ const getJwtToken = obj => jwt.sign(obj, process.env.APP_SECRET);
 
 const Mutation = {
   async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to do that!');
+    }
 
     const item = await ctx.db.mutation.createItem(
       {
-        data: { ...args }
+        data: {
+          // Relationship between the Item and the User
+          user: {
+            connect: {
+              id: ctx.request.userId
+            }
+          },
+          ...args
+        }
       },
       info
     );
