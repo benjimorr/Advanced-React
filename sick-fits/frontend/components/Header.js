3a31621c0264c5389560963497d9c6516add7ffc
Ben Morrison <benmorrison0@gmail.com>
2019-02-26 12:08:27 -0500

    Add ability to toggle cart side menu

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import Nav from './Nav';
+import Cart from './Cart';
 import styled from 'styled-components';
 
 Router.onRouteChangeStart = () => {
@@ -71,7 +72,7 @@ const Header = () => (
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 );
 
