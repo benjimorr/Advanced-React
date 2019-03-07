6560bac8d04d1e251d58713a6b190ebb2e7d5ebb
Ben Morrison <benmorrison0@gmail.com>
2019-03-07 16:20:02 -0500

    Begin work on checkout using stripe, start back end mutation to create order

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const { hasPermission } = require('../utils');
+const stripe = require('../stripe');
 
 const getJwtToken = obj => jwt.sign(obj, process.env.APP_SECRET);
 
@@ -254,6 +255,39 @@ const Mutation = {
       },
       info
     );
+  },
+  async createOrder(parent, args, ctx, info) {
+    // 1. Query the current user and make sure they are signed in
+    const { userId } = ctx.request;
+    if (!userId)
+      throw new Error('You must be signed in to complete this order!');
+    const user = await ctx.db.query.user(
+      { where: { id: userId } },
+      `{
+        id
+        name
+        email
+        cart {
+          id
+          quantity
+          item { id title price description image }
+        }}`
+    );
+    // 2. Recalculate the total for the price
+    const amount = user.cart.reduce(
+      (tally, cartItem) => tally + cartItem.item.price * cartItem.quantity,
+      0
+    );
+    // 3. Create the stripe charge (turn token into money)
+    const charge = await stripe.charges.create({
+      amount,
+      currency: 'USD',
+      source: args.token
+    });
+    // 4. Convert the CartItems to OrderItems
+    // 5. Create the Order
+    // 6. Clean up - clear the users cart, delete cartItems
+    // 7. Return the Order to the client
   }
 };
 
--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import { adopt } from 'react-adopt';
 import User from './User';
 import CartItem from './CartItem';
+import CheckOut from './CheckOut';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
@@ -61,7 +62,9 @@ const Cart = () => (
           </ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-            <SickButton>Checkout</SickButton>
+            <CheckOut>
+              <SickButton>Checkout</SickButton>
+            </CheckOut>
           </footer>
         </CartStyles>
       );
